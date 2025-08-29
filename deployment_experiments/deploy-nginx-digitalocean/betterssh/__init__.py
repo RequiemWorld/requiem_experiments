ab57cfda17cb2e2061564f757e0d8b058c07eb5d
polllaris <polllaris@protonmail.com>
2025-08-29 15:44:23 -0400

    add a script for deploying a directory with static content onto digitalocean with nginx and docker, and creating a cloudflare domain for it, expanding SSH helper code to support this in a semi-broken (but best effort for this time) capacity.

--- deployment_experiments/deploy-nginx-digitalocean/betterssh/__init__.py
+++ deployment_experiments/deploy-nginx-digitalocean/betterssh/__init__.py
@@ -1,5 +1,38 @@
+import os
 import paramiko
 from paramiko.rsakey import RSAKey
+from ._path_helper import LocalToRemoteFileWalker
+
+
+
+class SimpleSSHFiles:
+	def __init__(self, ssh_client: paramiko.SSHClient, debug_mode: bool = False):
+		self._ssh_client = ssh_client
+		self._debug_mode = debug_mode
+
+	def enable_debug_mode(self):
+		self._debug_mode = True
+
+	def upload_directory(self, local_path: str, remote_path: str):
+		sftp = self._ssh_client.open_sftp()
+		local_to_remote_walker = LocalToRemoteFileWalker(local_path, remote_path)
+		try:
+			for local_path, remote_path in local_to_remote_walker.walk():
+				remote_dir = os.path.dirname(remote_path)
+				try:
+					sftp.stat(remote_dir)
+					if self._debug_mode:
+						print(f"Directory already exists: {remote_dir}")
+				except FileNotFoundError:
+					if self._debug_mode:
+						print(f"Creating directory: {remote_dir}")
+					sftp.mkdir(remote_dir)
+
+				if self._debug_mode:
+					print(f"Uploading file: {local_path} -> {remote_path}")
+				sftp.put(local_path, remote_path)
+		finally:
+			sftp.close()
 
 
 class SimpleSSHClient:
@@ -9,9 +42,15 @@ class SimpleSSHClient:
 	def __init__(self, ssh_client: paramiko.SSHClient, debug_mode: bool = False):
 		self._ssh_client = ssh_client
 		self._debug_mode = debug_mode
+		self._file_management: SimpleSSHFiles = SimpleSSHFiles(ssh_client)
+
+	@property
+	def files(self) -> SimpleSSHFiles:
+		return self._file_management
 
 	def enable_debug_mode(self):
 		self._debug_mode = True
+		self._file_management.enable_debug_mode()
 
 	@classmethod
 	def from_host_and_key(cls, host: str, port: int, key: RSAKey, username: str) -> "SimpleSSHClient":
