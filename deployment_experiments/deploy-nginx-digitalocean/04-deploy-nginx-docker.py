768fba3b6ec71ab3a38acfdcd89bc892941a90a0
polllaris <polllaris@protonmail.com>
2025-08-29 11:17:28 -0400

    add the script for creating a server and starting a nginx docker container reliably
    This entire process (from the point of creating resources and setting them up with our own interfaces) should be redone with Rocky to see how much more complicated Ubuntu may be making this in comparison.

--- deployment_experiments/deploy-nginx-digitalocean/04-deploy-nginx-docker.py
+++ deployment_experiments/deploy-nginx-digitalocean/04-deploy-nginx-docker.py
@@ -8,14 +8,25 @@ from betterdo.extra import wait_for_tcp4_connectivity
 from betterdo.extra import IdempotentSSHKey
 from betterssh import SimpleSSHClient
 
+
+def wait_for_apt_lock_availability(ssh_client: SimpleSSHClient) -> None:
+	# -o DPkg::Lock::Timeout=5 and other amounts of seconds worked previously,
+	# it proved to be flaky on further deployments though, so this solution is preferred instead.
+	ssh_client.execute_command("while sudo lsof /var/lib/dpkg/lock-frontend; do echo 'Waiting for apt to finish...'; sleep 5; done")
+
+
 def install_docker_on_ubuntu_machine(ssh_client: SimpleSSHClient) -> None:
-	ssh_client.execute_command("apt update -y")
-	ssh_client.execute_command("apt install  -y " + " ".join(["apt-transport-https", "ca-certificates", "curl", "software-properties-common"]))
+	ssh_client.execute_command("apt-get -o DPkg::Lock::Timeout=50 update -y")
+	wait_for_apt_lock_availability(ssh_client)
+	ssh_client.execute_command("apt-get -o DPkg::Lock::Timeout=50 install  -y " + " ".join(["apt-transport-https", "ca-certificates", "curl", "software-properties-common"]))
+	wait_for_apt_lock_availability(ssh_client)
 	ssh_client.execute_command("curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg")
+	wait_for_apt_lock_availability(ssh_client)
 	ssh_client.execute_command('echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null')
 	# turns out that apt update is necessary after updating the repositories
-	ssh_client.execute_command("apt -o DPkg::Lock::Timeout=5 update -y")
-	ssh_client.execute_command("apt install docker-ce -y ")
+	ssh_client.execute_command("apt-get update -y")
+	wait_for_apt_lock_availability(ssh_client)
+	ssh_client.execute_command("apt-get  install docker-ce -y ")
 
 def create_nginx_docker_container_on_host(ssh_client: SimpleSSHClient, publish_to_port: int) -> None:
 	ssh_client.execute_command(f"docker run -d --name nginx -p {publish_to_port}:80 nginx:latest")
@@ -37,6 +48,8 @@ def main() -> None:
 	wait_for_tcp4_connectivity(server_ip_address, 22, try_for_seconds=120)
 	default_rsa_key = RSAKey.from_private_key_file(os.path.expanduser("~/.ssh/id_rsa"))
 	with SimpleSSHClient.from_host_and_key(host=server_ip_address, port=22, key=default_rsa_key, username="root") as ssh_client:
+		ssh_client.enable_debug_mode()
+		wait_for_apt_lock_availability(ssh_client)
 		install_docker_on_ubuntu_machine(ssh_client)
 		create_nginx_docker_container_on_host(ssh_client, 80)
 
